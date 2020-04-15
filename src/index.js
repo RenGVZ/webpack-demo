39aa160c38a138dce6e5f4c12ebc1c6ed66ac8db
gavingarza <gaving@live.com>
2020-04-15 21:28:19 +0900

    Trying to implement webpack javascript

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { initSortable } from './plugins/init_sortable'; 
+
+initSortable();
+
+// import _ from 'lodash';
+
+// function component() {
+//   const element = document.createElement('div');
+
+  
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//   return element;
+// }
+
+
+// document.body.appendChild(component());
---
+++ src/plugins/init_sortable.js
@@ -0,0 +1,14 @@
+import Sortable from 'sortablejs';
+
+
+
+const initSortable = () => {
+  const list = document.querySelector('#boxes');
+  Sortable.create(list, {
+    ghostClass: "ghost",
+    animation: 150,
+    
+  });
+};
+
+export { initSortable };
